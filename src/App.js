e870d0604375d2add184076cd4a861ffda2bb082
cameronDz <camerondziurgot@gmail.com>
2021-11-05 03:57:48 -0400

    set default height value

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 const copyStart = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
 const copyEnd= "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+const defaultHeight = 72;
 
 const ToggleButton = ({ isExpanded, onClick }) => {
   return (
@@ -13,16 +14,16 @@ const ToggleButton = ({ isExpanded, onClick }) => {
 
 const App = ({ isLongCopy = true }) => {
   const text = `${copyStart} ${isLongCopy ? copyEnd : ""}`;
-  const [heightCurrent, setHeightCurrent] = useState(72);
-  const [heightMax, setHeightMax] = useState(72);
-  const [heightMin, setHeightMin] = useState(72);
+  const [heightCurrent, setHeightCurrent] = useState(defaultHeight);
+  const [heightMax, setHeightMax] = useState(defaultHeight);
+  const [heightMin, setHeightMin] = useState(defaultHeight);
   const [isExpanded, setIsExpanded] = useState(false);
   const [isOverflow, setIsOverflow] = useState(false);
 
   useEffect(() => {
     const element = document.querySelector(".text-display")
-    const heightClient = element?.clientHeight || 0;
-    const scrollClient = element?.scrollHeight || 0;
+    const heightClient = element?.clientHeight || defaultHeight;
+    const scrollClient = element?.scrollHeight || defaultHeight;
     if (heightClient !== scrollClient) {
       setIsOverflow(true);
       setHeightMax(scrollClient);
